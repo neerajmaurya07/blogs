0ae1272e5decd8c7635cc1d94656eaf9e00adb86
Neeraj Maurya <neerajbittern104@gmail.com>
2017-03-19 17:17:42 +0530

    changes done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate_user!, only: [:new, :create,:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 end
