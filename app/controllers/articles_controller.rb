854f7ec5d02bca9e67fbb62a479d354d2bad366a
Neeraj Maurya <neerajbittern104@gmail.com>
2017-03-21 00:58:41 +0530

    changes  done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -72,6 +72,6 @@ class ArticlesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def article_params
-      params.require(:article).permit(:title, :text, :user_id)
+      params.require(:article).permit(:title, :text, :user_id, :tag_list)
     end
 end
