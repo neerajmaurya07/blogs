ae5456e93eb7bb51520c786d81a3f4931966ec35
Neeraj Maurya <neerajbittern104@gmail.com>
2017-03-25 15:39:27 +0530

    check list_to

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,14 +4,24 @@ class Article < ApplicationRecord
 	has_many :tags, through: :taggings
 	belongs_to :user
 
-	def tag_list
-	  self.tags.collect do |tag|
-	     tag.name
-	  end.join(", ")
+	
+
+	def self.tagged_with(name)
+	  Tag.find_by_name!(name).articles
 	end
-	def tag_list=(tags_string)
-	  tag_names = tags_string.split(",").collect{|s| s.strip.downcase}.uniq
-	  new_or_found_tags = tag_names.collect { |name| Tag.find_or_create_by(name: name) }
-	  self.tags = new_or_found_tags
+
+	def self.tag_counts
+	  Tag.select("tags.*, count(taggings.tag_id) as count").
+	    joins(:taggings).group("taggings.tag_id")
+	end
+
+	def tag_list
+	  tags.map(&:name).join(", ")
+	end
+
+	def tag_list=(names)
+	  self.tags = names.split(",").map do |n|
+	    Tag.where(name: n.strip).first_or_create!
+	  end
 	end
 end
